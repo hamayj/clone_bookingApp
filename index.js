f6912245806bbad174e3c8906a935428e752099c
hamayj <zmlzml0307@gmail.com>
2022-10-15 18:54:40 +0900

    fix: mongoDB 연결안되는 부분 우연히 해결. .env에서 문제가 있었던 듯

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ dotenv.config();
 // const mongoose = require('mongoose'); 이런 형식으로 써왔던 것 같은데 쓰니까 에러가 뜬다. 왜 그런거지. 이따 찾아보자.
 
 // initial connection
-// const connect = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO); // 우리가 암호화한 키를 연결해준 것.
-//         console.log("connected to mongoDB.");
-//       } catch (error) {
-//         throw error; // throw? 원래 정의돼 있는 함수야?
-//       } // 이 함수에서 막히는 줄 알고, 설마 오타..? 하면서 정화꺼 붙여넣어봤는데 똑같이 안먹음 ㅠㅠ
-// }
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO); // 우리가 암호화한 키를 연결해준 것.
+        //console.log("connected to mongoDB."); // 이거 굳이 설정안해도 하단부 코드에서 콘솔 띄워주네요.
+      } catch (error) {
+        throw error; // throw? 원래 정의돼 있는 함수야?
+      } // 이 함수에서 막히는 줄 알고, 설마 오타..? 하면서 정화꺼 붙여넣어봤는데 똑같이 안먹음 ㅠㅠ
+}
 
 mongoose.connection.on("disconnected", () => {
     console.log("mongoDB disconnected.")
@@ -26,7 +26,11 @@ mongoose.connection.on("connected", () => {
 }) // mongoDB에 연결된 경우
 
 
+app.get("/", (req, res) => {
+    res.send("hello first request")
+})
+
 app.listen(8000, () => {
-    connect() // 서버 연결될 때 몽구스도 함께 연결시켜주자.
-    console.log("connected to server.")
+    connect(); // 서버 연결될 때 몽구스도 함께 연결시켜주자.
+    console.log("connected to server.");
 });
